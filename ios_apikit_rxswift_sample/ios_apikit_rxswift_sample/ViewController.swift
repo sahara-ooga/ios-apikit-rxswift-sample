98c536d24ccbcb5ac3ff84a893f8d3017eff436c
sahara-ooga <y.ogasawara@st-ventures.jp>
2017-11-07 14:26:15 +0900

    generate new xcode project

---
+++ ios_apikit_rxswift_sample/ios_apikit_rxswift_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_apikit_rxswift_sample
+//
+//  Created by Yuu Ogasa on 2017/11/07.
+//  Copyright © 2017年 SundayCarpenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
